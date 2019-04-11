b5d947320831f590032ffa49ccde0d2c5899c946
YanWuling <zhangjingtun@gmail.com>
2019-04-11 10:57:22 +0800

    Lab2-2

--- 1_VerilogSourceCode/1_CPUCore_src/ControlUnit.v
+++ 1_VerilogSourceCode/1_CPUCore_src/ControlUnit.v
@@ -39,39 +39,71 @@ localparam AUIPC_OP = 7'b001_0111;
 localparam ALUI_OP  = 7'b001_0011;
 // 寄存器算术逻辑运算的操作码
 localparam ALUR_OP  = 7'b011_0011;
+// 跳转并连接操作码
+localparam JAL_OP   = 7'b110_1111;
+// 寄存器跳转并连接操作码
+localparam JALR_OP  = 7'b110_0111;
+// 分支指令操作码
+localparam BR_OP    = 7'b110_0011;
+// Load指令操作码
+localparam LOAD_OP  = 7'b000_0011;
+// Store指令操作码
+localparam STORE_OP = 7'b010_0011;
 //===============================================================================================
 // 具体指令信号
 // 移位指令
 wire SLLI, SRLI, SRAI, SLL, SRL, SRA;
-assign SLLI = (Op == ALUI_OP) && (Fn3 == 3'b001);
-assign SRLI = (Op == ALUI_OP) && (Fn3 == 3'b101) && (Fn7 == 7'b000_0000);
-assign SRAI = (Op == ALUI_OP) && (Fn3 == 3'b101) && (Fn7 == 7'b010_0000);
-assign SLL  = (Op == ALUR_OP) && (Fn3 == 3'b001);
-assign SRL  = (Op == ALUR_OP) && (Fn3 == 3'b101) && (Fn7 == 7'b000_0000);
-assign SRA  = (Op == ALUR_OP) && (Fn3 == 3'b101) && (Fn7 == 7'b010_0000);
+assign SLLI = (Op == ALUI_OP ) && (Fn3 == 3'b001);
+assign SRLI = (Op == ALUI_OP ) && (Fn3 == 3'b101) && (Fn7 == 7'b000_0000);
+assign SRAI = (Op == ALUI_OP ) && (Fn3 == 3'b101) && (Fn7 == 7'b010_0000);
+assign SLL  = (Op == ALUR_OP ) && (Fn3 == 3'b001);
+assign SRL  = (Op == ALUR_OP ) && (Fn3 == 3'b101) && (Fn7 == 7'b000_0000);
+assign SRA  = (Op == ALUR_OP ) && (Fn3 == 3'b101) && (Fn7 == 7'b010_0000);
 // 计算指令
 wire ADD, SUB, ADDI;
-assign ADD  = (Op == ALUR_OP) && (Fn3 == 3'b000) && (Fn7 == 7'b000_0000);
-assign SUB  = (Op == ALUR_OP) && (Fn3 == 3'b000) && (Fn7 == 7'b010_0000);
-assign ADDI = (Op == ALUI_OP) && (Fn3 == 3'b000);
+assign ADD  = (Op == ALUR_OP ) && (Fn3 == 3'b000) && (Fn7 == 7'b000_0000);
+assign SUB  = (Op == ALUR_OP ) && (Fn3 == 3'b000) && (Fn7 == 7'b010_0000);
+assign ADDI = (Op == ALUI_OP ) && (Fn3 == 3'b000);
 // 比较指令
 wire SLT, SLTU, SLTI, SLTIU;
-assign SLT  = (Op == ALUR_OP) && (Fn3 == 3'b010);
-assign SLTU = (Op == ALUR_OP) && (Fn3 == 3'b011);
-assign SLTI = (Op == ALUI_OP) && (Fn3 == 3'b010);
-assign SLTIU= (Op == ALUI_OP) && (Fn3 == 3'b011);
+assign SLT  = (Op == ALUR_OP ) && (Fn3 == 3'b010);
+assign SLTU = (Op == ALUR_OP ) && (Fn3 == 3'b011);
+assign SLTI = (Op == ALUI_OP ) && (Fn3 == 3'b010);
+assign SLTIU= (Op == ALUI_OP ) && (Fn3 == 3'b011);
 // 逻辑指令
 wire XOR, OR, AND, XORI, ORI, ANDI;
-assign XOR  = (Op == ALUR_OP) && (Fn3 == 3'b100);
-assign OR   = (Op == ALUR_OP) && (Fn3 == 3'b110);
-assign AND  = (Op == ALUR_OP) && (Fn3 == 3'b111);
-assign XORI = (Op == ALUI_OP) && (Fn3 == 3'b100);
-assign ORI  = (Op == ALUI_OP) && (Fn3 == 3'b110);
-assign ANDI = (Op == ALUI_OP) && (Fn3 == 3'b111);
+assign XOR  = (Op == ALUR_OP ) && (Fn3 == 3'b100);
+assign OR   = (Op == ALUR_OP ) && (Fn3 == 3'b110);
+assign AND  = (Op == ALUR_OP ) && (Fn3 == 3'b111);
+assign XORI = (Op == ALUI_OP ) && (Fn3 == 3'b100);
+assign ORI  = (Op == ALUI_OP ) && (Fn3 == 3'b110);
+assign ANDI = (Op == ALUI_OP ) && (Fn3 == 3'b111);
 // 立即数指令
 wire LUI, AUIPC;
 assign LUI  = (Op == LUI_OP  );
 assign AUIPC= (Op == AUIPC_OP);
+// 跳转与分支指令
+wire JAL, JALR, BEQ, BNE, BLT, BLTU, BGE, BGEU;
+assign JAL  = (Op == JAL_OP  );
+assign JALR = (Op == JALR_OP );
+assign BEQ  = (Op == BR_OP   ) && (Fn3 == 3'b000);
+assign BNE  = (Op == BR_OP   ) && (Fn3 == 3'b001);
+assign BLT  = (Op == BR_OP   ) && (Fn3 == 3'b100);
+assign BGE  = (Op == BR_OP   ) && (Fn3 == 3'b101);
+assign BLTU = (Op == BR_OP   ) && (Fn3 == 3'b110);
+assign BGEU = (Op == BR_OP   ) && (Fn3 == 3'b111);
+// Load指令
+wire LB, LH, LW, LBU, LHU;
+assign LB   = (Op == LOAD_OP ) && (Fn3 == 3'b000);
+assign LH   = (Op == LOAD_OP ) && (Fn3 == 3'b001);
+assign LW   = (Op == LOAD_OP ) && (Fn3 == 3'b010);
+assign LBU  = (Op == LOAD_OP ) && (Fn3 == 3'b100);
+assign LHU  = (Op == LOAD_OP ) && (Fn3 == 3'b101);
+// Store指令
+wire SB, SH, SW;
+assign SB   = (Op == STORE_OP) && (Fn3 == 3'b000);
+assign SH   = (Op == STORE_OP) && (Fn3 == 3'b001);
+assign SW   = (Op == STORE_OP) && (Fn3 == 3'b010);
 //===============================================================================================
 // 辅助信号
 // 在译码阶段由非load指令产生的寄存器写入标志
@@ -79,34 +111,48 @@ wire RegWD_NL = LUI || AUIPC || (Op == ALUR_OP) || (Op == ALUI_OP);
 //===============================================================================================
 // 各输出信号处理
 //------------------- JalD -------------------------
-assign JalD = 1'b0;
+assign JalD = JAL;
 //------------------- JalrD ------------------------
-assign JalrD= 1'b0;
+assign JalrD= JALR;
 //------------------- RegWriteD --------------------
 always @ (*)
     begin
-      if(RegWD_NL) RegWriteD <= `LW;
-      else RegWriteD <= `NOREGWRITE;
+      if(RegWD_NL)  RegWriteD <= `LW;
+      else if(LB)   RegWriteD <= `LB;
+      else if(LH)   RegWriteD <= `LH;
+      else if(LW)   RegWriteD <= `LW;
+      else if(LBU)  RegWriteD <= `LBU;
+      else if(LHU)  RegWriteD <= `LHU;
+      else          RegWriteD <= `NOREGWRITE;
     end
 //------------------- MemToRegD --------------------
-assign  MemToRegD = 1'b0;
+assign  MemToRegD = (Op == LOAD_OP);
 //------------------- MemWriteD --------------------
 always @ (*)
     begin
-        MemWriteD <= 4'b0000;
+        if(SB)      MemWriteD <= 4'b0001;
+        else if(SH) MemWriteD <= 4'b0011;
+        else if(SW) MemWriteD <= 4'b1111;
+        else        MemWriteD <= 4'b0000;
     end
 //------------------- LoadNpcD ---------------------
-assign LoadNpcD = 1'b0;
+assign LoadNpcD = JAL || JALR;
 //------------------- RegReadD ---------------------
 always @ (*)
     begin
-        RegReadD[0] <= (Op == ALUR_OP);
-        RegReadD[1] <= (Op == ALUI_OP) || (Op == ALUR_OP);
+        RegReadD[0] <= (Op == ALUR_OP) || (Op == BR_OP  ) || (Op == STORE_OP);
+        RegReadD[1] <= (Op == ALUI_OP) || (Op == ALUR_OP) || (Op == LOAD_OP ) || (Op == STORE_OP) || (Op == BR_OP) || JALR;
     end
 //------------------- BranchTypeD ------------------
 always @ (*)
     begin
-        BranchTypeD <= `NOBRANCH;
+        if(BEQ)         BranchTypeD <= `BEQ;
+        else if(BNE)    BranchTypeD <= `BNE;
+        else if(BLT)    BranchTypeD <= `BLT;
+        else if(BLTU)   BranchTypeD <= `BLTU;
+        else if(BGE)    BranchTypeD <= `BGE;
+        else if(BGEU)   BranchTypeD <= `BGEU;
+        else            BranchTypeD <= `NOBRANCH;
     end
 //------------------- AluContrlD -------------------
 always @ (*)
@@ -126,17 +172,20 @@ always @ (*)
     end
 //------------------- AluSrc2D ---------------------
 // 00 -- 寄存器；01 -- rs2的5位（移位操作那5位）；10 -- 立即数
-assign AluSrc2D = (SLLI || SRAI || SRLI) ? 2'b01 : ( (Op == ALUR_OP) ? 2'b00 : 2'b10 );
+assign AluSrc2D = (SLLI || SRAI || SRLI) ? 2'b01 : ( (Op == ALUR_OP || Op == BR_OP || Op == STORE_OP) ? 2'b00 : 2'b10 );
 //------------------- AluSrc1D ---------------------
 // 0 -- 寄存器；1 -- PC值
 assign AluSrc1D = AUIPC;
 //------------------- ImmType ----------------------
 always @ (*)
     begin
-        if      (Op == ALUR_OP)     ImmType <= `RTYPE;
-        else if (Op == ALUI_OP)     ImmType <= `ITYPE;
-        else if (LUI || AUIPC )     ImmType <= `UTYPE;
-        else                        ImmType <= 3'dx;
+        if      (Op == ALUR_OP)                             ImmType <= `RTYPE;
+        else if (Op == ALUI_OP || Op == LOAD_OP || JALR)    ImmType <= `ITYPE;
+        else if (LUI || AUIPC )                             ImmType <= `UTYPE;
+        else if (JAL)                                       ImmType <= `JTYPE;
+        else if (Op == BR_OP)                               ImmType <= `BTYPE;
+        else if (Op == STORE_OP)                            ImmType <= `STYPE;
+        else                                                ImmType <= 3'dx;
     end
 //--------------------------------------------------
 endmodule
